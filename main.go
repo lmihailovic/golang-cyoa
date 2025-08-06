55b0f124f77a62997533a622fbfd7ed757e20f5b
lmihailovic <miha.luka1006@gmail.com>
2025-08-07 00:23:21 +0200

    first working implementation

--- main.go
+++ main.go
@@ -2,10 +2,55 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"golang-cyoa/parse"
+	"log"
+	"net/http"
+	"strings"
 )
+
+func CreateMux(story parse.Story) http.Handler {
+	mux := http.NewServeMux()
+
+	for name, chapter := range story {
+		path := "/" + strings.ReplaceAll(name, " ", "-")
+		log.Println("Adding route for", path)
+
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintf(w, "<h1> %v </h1>", chapter.Title)
+
+			for _, paragraph := range chapter.Story {
+				fmt.Fprintf(w, "<p> %v </p>", paragraph)
+			}
+
+			for _, option := range chapter.Options {
+				fmt.Fprintf(w, "<p> <a href='/%v'>%v</a> </p>", option.Arc, option.Text)
+			}
+		})
+
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/intro", http.StatusFound)
+	})
+
+	return mux
+}
 
 func main() {
 	storyFile := flag.String("f", "gopher.json", "path to story file")
 
+	story, err := parse.LoadStory(*storyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := CreateMux(story)
+
+	println("Listening on port 8080")
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- parse/parse.go
+++ parse/parse.go
@@ -1,5 +1,43 @@
 package parse
 
-func LoadStory(path string) {
+import (
+	"encoding/json"
+	"os"
+)
 
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title   string    `json:"title"`
+	Story   []string  `json:"story"`
+	Options []Options `json:"options"`
+}
+
+type Options struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+func LoadStory(file string) (Story, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+		return nil, err
+	}
+
+	var story Story
+
+	err = json.Unmarshal(bytes, &story)
+
+	//for _, v := range story {
+	//	fmt.Printf("\n\n== %v ==\n", v.Title)
+	//	for _, s := range v.Story {
+	//		println(s)
+	//	}
+	//	for index, o := range v.Options {
+	//		fmt.Printf("\nPress %v to %v", index+1, o.Text)
+	//	}
+	//}
+
+	return story, nil
 }
